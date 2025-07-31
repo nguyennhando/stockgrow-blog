7f56c3a77bc6cae8d5e4cfbb0df657a9532991aa
nguyennhando <120247762+nguyennhando@users.noreply.github.com>
2025-07-31 16:39:16 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAecworkmBW-GQCKVKM2ro0AF2SqHTWDik",
+  authDomain: "stockgrow-blog.firebaseapp.com",
+  projectId: "stockgrow-blog",
+  storageBucket: "stockgrow-blog.firebasestorage.app",
+  messagingSenderId: "406881388796",
+  appId: "1:406881388796:web:897c9ef1300afab35e6f79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
